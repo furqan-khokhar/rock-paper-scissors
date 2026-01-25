cffb4fadbf15c4b500d8c2b026f8df5364e9eb69
Furqan Khokhar <furqan.a.khokhar@gmail.com>
2026-01-25 21:14:59 +0000

    feat: upgraded html to ws and added html
    upgraded html to websocket and made simple webpage that is run with live-server to check connection - everything seems to be working

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/rock-paper-scissors
 
 go 1.25.5
+
+require github.com/gorilla/websocket v1.5.3
--- main.go
+++ main.go
@@ -10,7 +10,46 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
+
+// upgrader is a struct that will
+// take incoming http request
+// check if it is a valid ws request
+// perform the ws handshake
+// return a pointer to a websocket connection (*websocket.Conn)
+var upgrader = websocket.Upgrader{
+	// data that can be read/written (in bytes per frame)
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	// validates http request to be upgraded to ws connection
+	CheckOrigin: func(r *http.Request) bool { return true },
+}
+
+func reader(conn *websocket.Conn) {
+	// infinite loop
+	for {
+		// tries to read a message from ws conn
+		messageType, p, err := conn.ReadMessage()
+		// if error
+		if err != nil {
+			// log error and return
+			log.Println(err)
+			return
+		}
+
+		// log message
+		log.Println(string(p))
+
+		// write message back to ws client (echo server)
+		if err := conn.WriteMessage(messageType, p); err != nil {
+			// if there was an error in writing than print it and return
+			log.Println(err)
+			return
+		}
+	}
+}
 
 // responds to home request
 func homePage(w http.ResponseWriter, r *http.Request) {
@@ -19,7 +58,28 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 
 // responds to websocket endpoint request
 func wsEndpoint(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World")
+	// upgrades http to websocket connection
+	ws, err := upgrader.Upgrade(w, r, nil)
+	// if there's an error
+	if err != nil {
+		// log error
+		log.Println(err)
+	}
+
+	// otherwise connection is successful
+	log.Println("Client Connected")
+
+	// send message from server to client
+	err = ws.WriteMessage(1, []byte("Hi Client!"))
+	// if there's an error
+	if err != nil {
+		// log error
+		log.Println(err)
+	}
+
+	// listen indefinitely for new messages
+	// through ws connection
+	reader(ws)
 }
 
 // maps urls to functions
@@ -35,11 +95,12 @@ func main() {
 	fmt.Println("Hello World")
 	// sets up routes/endpoints
 	setupRoutes()
-	// http.ListenAndServe
+	// http.ListenAndServe :
 	// starts http server on port 8080
 	// listen for and serve http requests indefinitely
 	// return if there is an error
-	// log.Fatal
+
+	// log.Fatal :
 	// logs error
 	// force exits program
 	log.Fatal(http.ListenAndServe(":8080", nil))
